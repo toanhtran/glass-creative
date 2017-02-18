6ce58344651f46db8b6f25ea20ab9d07d6c96868
toanhtran <toanh.t.tran@gmail.com>
2017-02-17 21:55:24 -0800

    sidebar widget

--- wp-content/themes/glass-creative/functions.php
+++ wp-content/themes/glass-creative/functions.php
@@ -35,3 +35,19 @@ function glasseye_scripts() {
     wp_enqueue_style('glasseye_google_fonts', 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700');
 }
 add_action('wp_enqueue_scripts', 'glasseye_scripts');
+
+/* Register Widget Area */
+
+function glasseye_widget_init() {
+    
+    register_sidebar( array (
+        'name' => __('Main Sidebar', 'glasseye' ),
+        'id' => 'main-sidebar',
+        'description' => __('Widgets added here will appear in all pages using two-column template', 'glasseye'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        "before_title" => '<h2 class="widget-title">',
+        'after-title' => '</h2>'
+    ));
+}
+add_action('widgets_init', 'glasseye_widget_init' );
